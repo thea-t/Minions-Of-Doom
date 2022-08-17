490db214214226802843af866960771e87d9f83e
thea-t <teodoratx3@gmail.com>
2022-08-17 21:06:02 +0100

    enemies created

--- Assets/MOF/Scripts/Minions/CharacterLookChanger.cs
+++ Assets/MOF/Scripts/Minions/CharacterLookChanger.cs
@@ -12,24 +12,32 @@ public class CharacterLookChanger : MonoBehaviour {
     [SerializeField] private Transform m_BackTransform;
     [SerializeField] private Transform m_LeftHandTransform;
     [SerializeField] private Transform m_RightHandTransform;
-    
+
+
+    // Adds this function to the menu options so that you can call it in the editor. This way you can check its look 
+    [ContextMenu("Set look")]
     public void SetCharacterLook()
     {
-        if (m_MeshRenderer.material != null)
+        if (m_MeshRenderer.sharedMaterial != null)
         {
-            m_MeshRenderer.material = m_CharacterLookData.minionMaterial;
+            m_MeshRenderer.sharedMaterial = m_CharacterLookData.minionMaterial;
         }
         else
         {
             Debug.LogError("Warning! Skin material is null for: " + gameObject.name);
         }
 
+        ClearPrevious();
+
 
         if (m_CharacterLookData.head.Length > 0)
         {
             for (int i = 0; i < m_CharacterLookData.head.Length; i++)
             {
-                Instantiate(m_CharacterLookData.head[i], m_HeadTransform);
+                if (m_CharacterLookData.head[i] != null)
+                {
+                    Instantiate(m_CharacterLookData.head[i], m_HeadTransform);
+                }
             }
         }
 
@@ -45,6 +53,26 @@ public class CharacterLookChanger : MonoBehaviour {
         {
 
             Instantiate(m_CharacterLookData.rightHand, m_RightHandTransform);
+        }
+    }
+
+    private void ClearPrevious()
+    {
+        foreach (Transform child in m_HeadTransform)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+        foreach (Transform child in m_BackTransform)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+        foreach (Transform child in m_LeftHandTransform)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+        foreach (Transform child in m_RightHandTransform)
+        {
+            DestroyImmediate(child.gameObject);
         }
     }
 }
